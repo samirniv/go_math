9d5f8a5926a698c5619f5ff8a2a18d8546142ea9
samir <samir_chakrabarti@hotmail.com>
2020-09-22 22:24:12 +0530

    zero edge is not allowed

--- geometry/cube.go
+++ geometry/cube.go
@@ -1,5 +1,15 @@
 package geometry
 
-func CubeVolume(n int) int  {
-	return n * n * n
+import (
+	"errors"
+)
+
+func CubeVolume(n int) (int, error) {
+	if n != 0 {
+		return n * n * n, nil
+	} else {
+		return 0, errors.New("Zero length is not allowed")
+	}
 }
+
+//vMAJOR.MINOR.PATCH -> this is the numbering scheme used go modules
